ead7aaf5ae158c011a521adf73dfd85e19a80db7
Gabrieleirbag1 <gabrielgarrone670@gmail.com>
2025-07-09 15:50:29 +0200

    Refactor logging configuration and color handling; move LOGGING_ENABLED and LOG_LEVEL to config.py and enhance log color management in log_colors.py

--- lite_logging/lite_logging.py
+++ lite_logging/lite_logging.py
@@ -1,42 +1,11 @@
 from typing import Optional
 import inspect
+from lite_logging.config import LogColors, LOGGING_ENABLED, LOG_LEVEL
 
-LOGGING_ENABLED = True
-DEBUG_MODE = True
-LOG_LEVEL = ["INFO", "DEBUG", "WARNING", "ERROR"]
-
-class LogColors:
-    """Colors for log messages
-    
-    :param str HEADER: Header color
-    :param str OKBLUE: Blue color
-    :param str OKCYAN: Cyan color
-    :param str OKGREEN: Green color
-    :param str WARNING: Warning color
-    :param str FAIL: Fail color
-    :param str ENDC: End color
-    :param str OKYELLOW: Yellow color
-    :param str OKMAGENTA: Magenta color
-    :param str OKWHITE: White color
-    :param str BOLD: Bold color
-    :param str UNDERLINE: Underline"""
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKCYAN = '\033[96m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    OKYELLOW = '\033[93m'
-    OKMAGENTA = '\033[35m'
-    OKWHITE = '\033[97m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
-
-def get_message(message: str, level: Optional[str]) -> Optional[tuple[str, str]]:
+def get_message(message: tuple, level: Optional[str]) -> Optional[tuple[str, str]]:
     """Returns the message and color for the log message
 
-    :param str message: The message to print
+    :param tuple message: The message to print
     :param Optional[str] level: The log level.
     
     :return: The message and color for the log message
@@ -44,16 +13,11 @@ def get_message(message: str, level: Optional[str]) -> Optional[tuple[str, str]]
     level = level.upper()
     if not LOGGING_ENABLED:
         return None
-    if level not in LOG_LEVEL:
+    
+    if level not in LOG_LEVEL or not LOG_LEVEL[level]["enabled"]:
         return None
-    if level == "DEBUG" and not DEBUG_MODE:
-        return None
-    color = {
-        "INFO": LogColors.OKGREEN,
-        "WARNING": LogColors.WARNING,
-        "ERROR": LogColors.FAIL,
-        "DEBUG": LogColors.OKCYAN
-    }.get(level, LogColors.ENDC)
+        
+    color = LOG_LEVEL[level]["color"]
     message_str = ' '.join(map(str, message))
     return message_str, color
 
